1016c0d2ce808b5897e098711616c87b5fe2bfa1
sabryp3 <sabryp3@yahoo.com>
2025-12-04 15:00:05 +0000

    bucket ammend

--- api/main.py
+++ api/main.py
@@ -27,9 +27,11 @@ app.add_middleware(
 s3 = boto3.client(
     's3',
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"),
+    region_name= os.getenv("AWS_REGION"),
+)
 
-bucket_name = os.getenv(S3_BUCKET_NAME) # Add your bucket name here
+bucket_name = os.getenv("S3_BUCKET_NAME") # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
