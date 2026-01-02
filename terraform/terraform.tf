bc341dbd577af0e575053e035edb9c033d738bfc
sabryp3 <sabryp3@yahoo.com>
2026-01-02 16:26:28 +0000

    fixing syntax

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.7"
+      version = "~> 6.7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -23,4 +23,4 @@ terraform {
 
     }
   }
-}
+
