58481661b1879f770fff70c2e79c474d294eeba4
sabryp3 <sabryp3@yahoo.com>
2026-01-20 14:56:10 +0000

    arranging terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,24 @@
 terraform {
+  required_version = "~> 1.0"
+  backend "s3" {
+    bucket         = "infrabucket-p3"
+    key            = "terraform/state.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "tf-db-table"
+    encrypt        = true
+  }
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.7"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = "~> 2.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16"
     }
   }
 }
@@ -21,4 +37,3 @@ provider "kubernetes" {
 provider "aws" {
   region = var.aws_region
 }
-
