fe3c6631c249d04da1d0f0c48f33db2d5331d251
sabryp3 <sabryp3@yahoo.com>
2026-01-23 16:33:13 +0000

    adding eks endpoint public access

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -21,8 +21,7 @@ module "eks" {
     }
   }
 
-  # Optional
-  endpoint_public_access = true
+ 
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
