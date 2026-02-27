65b64109690caf60ec85fc4f32402170d99af57e
sabryp3 <sabryp3@yahoo.com>
2026-02-27 12:33:20 +0000

    simplify OIDC

--- terraform/OIDC/output.tf
+++
@@ -1,5 +0,0 @@
-# Output the role ARN for use in GitHub Actions workflow
-output "github_actions_role_arn" {
-  value       = aws_iam_role.github_deploy.arn
-  description = "ARN of the role for GitHub Actions to assume"
-}
